556d0f2ae57a464c83fa8ca3b034b6e38cb34c33
Adam O' Ceallaigh <oceallaighadam880@gmail.com>
2021-03-13 15:51:18 +0000

    Updated to the new slots way

--- DB/Models/clinic.js
+++ DB/Models/clinic.js
@@ -2,8 +2,12 @@ const mongoose = require('../connection'),
 clinicSchema = new mongoose.Schema({
     Month: String,
     Dates: [String],
-    Hours: [String],
-    Providers: String
+    Timeslots: [
+        {
+            "Hours": [String],
+            "Providers": String
+        }
+    ]
 }),
 clinic = mongoose.model("Clinic", clinicSchema)
 module.exports = clinic
